15c0f6482aff1c7249897e1008d9ad12bb729390
BoopPush <ld01@tut.by>
2021-11-03 12:25:37 +0300

    add validation by average mark

--- src/main/java/com/example/lab2_attempt2/MainActivity.java
+++ src/main/java/com/example/lab2_attempt2/MainActivity.java
@@ -15,18 +15,25 @@ import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Enrollee> enrollees = new ArrayList();
+    ArrayList<Enrollee> validEnrollees = new ArrayList();
     ListView enrolleeList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setInitialData();
+        for (int j = 0;j<enrollees.size();j++){
+            if (enrollees.get(j).averageMark() > getAverageUni(enrollees))
+            {
+                validEnrollees.add(enrollees.get(j));
+            }
+        }
         enrolleeList = findViewById(R.id.enrolleeList);
         TextView textView = findViewById(R.id.uni_average);
-        textView.setText("Average university mark: "+getAverageUni(enrollees));
-        EnrolleeAdapter enrolleeAdapter = new EnrolleeAdapter(this,R.layout.list_enrollee,enrollees);
+        textView.setText("Average university mark: "+getAverageUniString(enrollees));
+        EnrolleeAdapter enrolleeAdapter = new EnrolleeAdapter(this,R.layout.list_enrollee,validEnrollees);
         enrolleeList.setAdapter(enrolleeAdapter);
-        Collections.sort(enrollees,Enrollee.EnrolleeMarkDescendingComparator);
+        Collections.sort(validEnrollees,Enrollee.EnrolleeMarkDescendingComparator);
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -49,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         enrollees.add(new Enrollee("Eva","Vladimirovna","Lozko",10,9,10));
     }
 
-    private String getAverageUni(ArrayList<Enrollee> enrollees){
+    private String getAverageUniString(ArrayList<Enrollee> enrollees){
         Double sum = 0.0;
         for (int i = 0;i< enrollees.size();i++){
             sum+= enrollees.get(i).averageMark();
@@ -58,5 +65,14 @@ public class MainActivity extends AppCompatActivity {
         sum/=enrollees.size();
         String result = String.format("%.1f",sum);
         return result;
+    }
+    private Double getAverageUni(ArrayList<Enrollee> enrollees){
+        Double sum = 0.0;
+        for (int i = 0;i< enrollees.size();i++){
+            sum+= enrollees.get(i).averageMark();
+        }
+
+        sum/=enrollees.size();
+        return sum;
     }
 }
